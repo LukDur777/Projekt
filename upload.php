8a7c063eff67e6f2f65c4ffd737520607d9260d1
LukDur777 <durawalukasz@wp.pl>
2024-02-26 13:29:25 +0100

    .

--- upload.php
+++ upload.php
@@ -7,16 +7,33 @@ if(!empty($_POST)) {
     //zdefiniuj folder docelowy
     $targetDirectory = "img/";
     //uzyj oryginalnej nazwy pliku
-    $fileName = $_FILES['file']['name'];
+    //$fileName = $_FILES['file']['name'];
+    
+    $fileName = hash('sha356', $_FILES['file']['name'].microtime());
+
+    //uzyj oryginalnej nazwy pliku
     //przesun plik z lokalizacji tymczasowej do docelowej
-    move_uploaded_file($_FILES['file']['tmp_name'], $targetDirectory.$fileName);
+    //move_uploaded_file($_FILES['file']['tmp_name'], $targetDirectory.$fileName);
+
+    //[1] wczytaj zawartosc pliku graficznego do stringa
+    $fileString = file_get_contents($_FILES['file']['tmp_name']);
+
+    //[2] wczytaj otrzymany z formularza obrazek uzywajac biblioteki GD do obiektu klasa GDimage
+    $gdImage = imagecreatefromgd($_FILES['files']['tmp_name']);
+
+//przygotuj pelny url pliku
+    $finalUrl = "http://localhost/loremm/img" .$fileName.".webp";
+
+    //[3] zapisz obraz skonwertowany do webp pod nazwa pliku + rozszerzenie webp
+    imagewebp($gdImage, )
 
     //dopisz posta do bazy
     //tymczasowo = authorID
     $authorID = 1;
+
     $imageUrl = "localhost/lorem/img/" . $fileName;
 
-    $db = new mysqli('localgost','root','','lorem' );
+    $db = new mysqli('localhost','root','','lorem' );
     $q = $db->prepare("INSERT INTO post (author, imgUrl, title)VALUES (?, ?, ?)");
 
     //pierwszy atrybut jest liczba, dwa pozostale tekstem wiec integer string string
